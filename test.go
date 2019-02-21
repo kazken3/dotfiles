4db8295d65ec30b0bc1deb8d596e630ba212beb0
Kentaro KAZUHAMA <kazken3@gmail.com>
2019-02-21 23:44:45 +0900

    untracked files on master: 38276de Update

---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "runtime"
+
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println("vim-go")
+	fmt.Println(runtime.GOARCH)
+}
---
+++ test.py
@@ -0,0 +1,8 @@
+#/usr/bin/python
+
+import os
+
+print(os.environ["PWD"])
+
+
+
